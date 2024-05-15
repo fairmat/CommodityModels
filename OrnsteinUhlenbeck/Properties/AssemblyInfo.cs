60df94aa0d384472d0cd3f2c73b1c3e41d5ccc44
Mattia Trevisani <mattia.trevisani@fairmat.com>
2024-05-15 12:07:52 +0200

    [RD-FFK-NET8] Applied some fixes while validating. fairmat/dev/fairmatserver#3493

--- OrnsteinUhlenbeck/Properties/AssemblyInfo.cs
+++ OrnsteinUhlenbeck/Properties/AssemblyInfo.cs
@@ -16,20 +16,10 @@
  */
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using Mono.Addins;
+
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6cdffdf3-5631-41e9-9761-b268a44265b9")]
-
 
 // The following lines tell that the assembly is an addin
 [assembly: Addin("Ornstein Uhlenbeck", "0.9.3", Category = "Calibration")]
--- SchwartzSmithModel/Properties/AssemblyInfo.cs
+++ SchwartzSmithModel/Properties/AssemblyInfo.cs
@@ -1,13 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b39555a1-2e6b-46cb-8c25-71d00fa3e40d")]
